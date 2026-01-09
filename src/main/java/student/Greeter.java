b5ecd153ecfce9c8c777fb09c2b285d30b069d2f
Emmitt Tucker <ejtfrogman@gmail.com>
2026-01-09 17:17:23 -0500

    Made the greeter list index 0 based instead pf 1 based

--- src/main/java/student/Greeter.java
+++ src/main/java/student/Greeter.java
@@ -73,7 +73,7 @@ public class Greeter {
      * @return the name of the greeter
      */
     public String getName() {
-        return name
+        return name;
     }
 
     /**
@@ -192,11 +192,11 @@ public class Greeter {
         if (locality < 1 || locality >= localityList.size()) {
             return "USA";
         }
-        return localityList.get(locality);
+        return localityList.get(locality - 1);
     }
 
     /**
-     * For new objects is is often a good idea to override the hashCode method.
+     * For new objects is often a good idea to override the hashCode method.
      *
      * HashCodes are used in various data structures (like hashtables) to provide a 'unique'
      * identifier for an object. In this case as long as the name and locality are the same, the
